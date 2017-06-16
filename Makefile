c20bbca9e6c47a80183b51b5382b75fbe698aaf0
An Do <a.do@laudert.de>
2017-06-16 18:19:18 +0700

    Initializing

---
+++ Makefile
@@ -0,0 +1,47 @@
+SHELL=/bin/bash
+
+CC=cc
+CFLAGS+=-O2 -std=c89 -Wpedantic -Wall -Wextra -Wunused -Wshadow -Wdouble-promotion -Wstrict-overflow=5
+
+INSTALL=install -D
+INSTALL_DATA=$(INSTALL) -m 644
+
+BINDIR=$(HOME)/bin
+#MANDIR=$(DESTDIR)/usr/share/man/man6
+
+STRIP=strip
+ifeq ($(OS),Windows_NT)
+ X = .exe
+else
+  ifeq ($(shell uname), SunOS)
+    STRIP=gstrip
+  endif
+endif
+
+PROG=giit$X
+MANPAGE=giit.6.gz
+
+$(PROG): *.c
+	$(CC) -o $@ $(CPPFLAGS) $(CFLAGS) $(LDFLAGS) $^
+#	-$(STRIP) -s $@
+
+$(MANPAGE): giit.6
+	gzip -9 -n -c giit.6 > giit.6.gz
+
+install: $(PROG) $(MANPAGE)
+	$(INSTALL) $(PROG) $(BINDIR)/$(PROG)
+#	$(INSTALL_DATA) $(MANPAGE) $(MANDIR)/$(MANPAGE)
+
+uninstall:
+	rm -f $(BINDIR)/$(PROG)
+#	rm -f $(MANDIR)/$(MANPAGE)
+
+fmt: *.c
+	VERSION_CONTROL=never indent -kr -i4 -ppi4 -nut -l100 -cp0 -ncs -sob \
+		-T HANDLE \
+		$^
+
+.PHONY: clean install uninstall
+clean:
+	rm -f $(PROG)
+#	rm -f $(MANPAGE)
---
+++ giit.c
@@ -0,0 +1,319 @@
+/*
+ * giit - a git launcher
+ *
+ */
+
+#if defined (_WIN32) && !defined(WIN32)
+#    define WIN32
+#else
+     /* fileno() */
+#    define _POSIX_C_SOURCE 1
+     /* usleep() */
+#    define _DEFAULT_SOURCE
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(WIN32) && !defined(__CYGWIN__)
+#    include <process.h>
+#    include <io.h>
+
+    /* usleep() doesn't exist on MSVC, instead use Sleep() from Win32 API */
+#    define usleep(a) Sleep((a) / 1000)
+
+    /*
+     * exec*() on MSVC makes the parent process exit; that means that giit.exe will finish as git is starting,
+     * which causes cmd.exe to print its prompt over git's output (because it sees that the child process has
+     * finished). The solution is to use synchronous spawn*(): it will make giit.exe to wait until git finishes.
+     */
+#    define execv(a, b) do { i = _spawnv(_P_WAIT, (a), (b)); if (i != -1) return i; } while(0)
+#    define execvp(a, b) do { i = _spawnvp(_P_WAIT, (a), (b)); if (i != -1) return i; } while(0)
+
+#else
+#    include <unistd.h>
+#endif
+
+#ifndef WIN32
+#    include <sys/ioctl.h>
+#else
+#    include <windows.h>
+HANDLE WIN_CONSOLE;
+#endif
+
+/* SunOS defines winsize in termios.h */
+#if defined(__sun) && defined(__SVR4)
+#    include <sys/termios.h>
+#endif
+
+#define GIT_NAME "git"
+
+#ifndef giit_SPEED
+#    define giit_SPEED 1000
+#endif
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+int term_width(void);
+void init_space(void);
+void open_term();
+void move_to_top(void);
+void line_at(int start_x, const char *s);
+void clear_car(int x);
+
+typedef void (*draw_fn_t) (int x);
+void draw_std(int x);
+void draw_push(int x);
+void draw_pull(int x);
+draw_fn_t select_command(int argc, char **argv);
+
+FILE *TERM_FH;
+int TERM_WIDTH;
+unsigned int FRAME_TIME;
+unsigned int PUSH_MOVE[] = {0, 1, 2, 3, 4, 3, 2, 1};
+
+int main(int argc, char **argv)
+{
+    int i;
+    char *git_path;
+    char *tmp;
+    unsigned int giit_speed;
+    draw_fn_t draw_fn;
+
+    tmp = getenv("giit_SPEED");
+    if (!tmp || sscanf(tmp, "%u", &giit_speed) != 1) {
+        giit_speed = giit_SPEED;
+    }
+    open_term();
+    TERM_WIDTH = term_width();
+    FRAME_TIME = 1000 * 1000 * 10 / (giit_speed + TERM_WIDTH + 1);
+
+    draw_fn = select_command(argc, argv);
+    init_space();
+    for (i = 0; i < TERM_WIDTH; i++) {
+    //for (i = -20; i < TERM_WIDTH; i++) {
+        draw_fn(i);
+        clear_car(i);
+    }
+    move_to_top();
+    fflush(TERM_FH);
+
+    git_path = getenv("GIT");
+    if (git_path) {
+        execv(git_path, argv);
+    } else {
+        execvp(GIT_NAME, argv);
+    }
+    /* error in exec if we land here */
+    perror(GIT_NAME);
+    return 1;
+}
+
+draw_fn_t select_command(int argc, char **argv)
+{
+    int i;
+
+    for (i = 1; i < argc; i++) {
+        if (argv[i][0] == '-')
+            continue;
+        if (!strcmp(argv[i], "push"))
+            return draw_push;
+        if (!strcmp(argv[i], "pull"))
+            return draw_pull;
+        break;
+    }
+    return draw_std;
+}
+
+void init_space(void)
+{
+    fputs("\n\n\n\n\n\n\n", TERM_FH);   /* 8 lines, to not remove the PS1 line */
+    fflush(TERM_FH);
+}
+
+void open_term()
+{
+#ifndef WIN32
+    TERM_FH = fopen("/dev/tty", "w");
+    if (!TERM_FH)
+        TERM_FH = stdout;
+#else
+    TERM_FH = fopen("CONOUT$", "w+");
+    WIN_CONSOLE = (HANDLE)_get_osfhandle(fileno(TERM_FH));
+#endif
+}
+
+int term_width(void)
+{
+#ifndef WIN32
+    struct winsize w;
+    ioctl(fileno(TERM_FH), TIOCGWINSZ, &w);
+    return w.ws_col;
+#else
+    CONSOLE_SCREEN_BUFFER_INFO ci;
+    GetConsoleScreenBufferInfo(WIN_CONSOLE, &ci);
+    return ci.dwSize.X;
+#endif
+}
+
+void move_to_top(void)
+{
+#ifndef WIN32
+    fprintf(TERM_FH, "\033[7A");
+#else
+    CONSOLE_SCREEN_BUFFER_INFO ci;
+    GetConsoleScreenBufferInfo(WIN_CONSOLE, &ci);
+    ci.dwCursorPosition.X = 0;
+    ci.dwCursorPosition.Y -= 7;
+    SetConsoleCursorPosition(WIN_CONSOLE, ci.dwCursorPosition);
+#endif
+}
+
+void move_to_x(int x)
+{
+#ifndef WIN32
+    fprintf(TERM_FH, "\033[%dC", x);
+#else
+    CONSOLE_SCREEN_BUFFER_INFO ci;
+    GetConsoleScreenBufferInfo(WIN_CONSOLE, &ci);
+    ci.dwCursorPosition.X = x;
+    SetConsoleCursorPosition(WIN_CONSOLE, ci.dwCursorPosition);
+#endif
+}
+
+void line_at(int start_x, const char *s)
+{
+    int x;
+    size_t i;
+    if (start_x > 1)
+        move_to_x(start_x);
+    for (x = start_x, i = 0; i < strlen(s); x++, i++) {
+        if (x > 0 && x < TERM_WIDTH)
+            fputc(s[i], TERM_FH);
+    }
+#ifdef WIN32
+    /*
+     * It seems Windows wraps on whe cursor when it's about to overflow,
+     * rather than after it has overflown (unless the overflowing character
+     * is a newline), as other systems seems to do.
+     */
+    if (x < TERM_WIDTH)
+#endif
+        fputc('\n', TERM_FH);
+
+    fflush(TERM_FH);
+}
+
+void draw_std(int x)
+{
+    /* *INDENT-OFF* */
+    move_to_top();
+    line_at(x, "   ,---------------.");
+    line_at(x, "  /  /``````|``````\\\\");
+    line_at(x, " /  /_______|_______\\\\________");
+    line_at(x, "|]      giit |'       |        |]");
+    if (x % 2) {
+    line_at(x, "=  .-:-.    |________|  .-:-.  =");
+    line_at(x, " `  -+-  --------------  -+-  '");
+    line_at(x, "   '-:-'                '-:-'  ");
+    } else {
+    line_at(x, "=  .:-:.    |________|  .:-:.  =");
+    line_at(x, " `   X   --------------   X   '");
+    line_at(x, "   ':-:'                ':-:'  ");
+    }
+    /* *INDENT-ON* */
+    usleep(FRAME_TIME);
+}
+
+void draw_push(int x)
+{
+    /* *INDENT-OFF* */
+    move_to_top();
+    line_at(x, "   __      ,---------------.");
+    line_at(x, "  /--\\   /  /``````|``````\\\\");
+    line_at(x, "  \\__/  /  /_______|_______\\\\________");
+    line_at(x, "   ||-< |]      giit |'       |        |]");
+    if (x % 2) {
+    line_at(x, "   ||-< =  .-:-.    |________|  .-:-.  =");
+    line_at(x, "   ||    `  -+-  --------------  -+-  '");
+    line_at(x, "   ||      '-:-'                '-:-'  ");
+    } else {
+    line_at(x, "   ||-< =  .:-:.    |________|  .:-:.  =");
+    line_at(x, "   /\\    `   X   --------------   X   '");
+    line_at(x, "  /  \\     ':-:'                ':-:'  ");
+    }
+    /* *INDENT-ON* */
+    usleep(FRAME_TIME * 10);
+}
+
+void draw_pull(int x)
+{
+    int x1 = PUSH_MOVE[x % 8];
+    move_to_top();
+    char buff[100];
+    line_at(x, "                                       ");
+    line_at(x, "                                       ");
+    if (x1 == 0) {
+        sprintf(buff, "             %s~%s=[,,_,,]:3               ", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+        line_at(x, buff);
+        sprintf(buff, "                                       ");
+        line_at(x, buff);
+        sprintf(buff, "     %s|%s                                 ", ANSI_COLOR_YELLOW, ANSI_COLOR_RESET);
+        line_at(x, buff);
+        sprintf(buff, "   %s--*--%s                               ", ANSI_COLOR_YELLOW, ANSI_COLOR_RESET);
+        line_at(x, buff);
+        sprintf(buff, "     %s|%s                                 ", ANSI_COLOR_YELLOW, ANSI_COLOR_RESET);
+        line_at(x, buff);
+    } else if (x1 == 1) {
+        line_at(x, "                                *      ");
+        sprintf(buff, "             %s~%s=[,,_,,]:3               ", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+        line_at(x, buff);
+        line_at(x, "                                       ");
+        sprintf(buff, "     %s*%s                                 ", ANSI_COLOR_GREEN, ANSI_COLOR_RESET);
+        line_at(x, buff);
+        line_at(x, "                                       ");
+    } else if (x1 == 2) {
+        line_at(x, "                                       ");
+        sprintf(buff, "                                 %s.%s     ", ANSI_COLOR_BLUE, ANSI_COLOR_RESET);
+        line_at(x, buff);
+        sprintf(buff, "             %s~%s=[,,_,,]:3               ", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+        line_at(x, buff);
+        line_at(x, "     .                                 ");
+        line_at(x, "                                       ");
+    } else if (x1 == 3) {
+        line_at(x, "                                       ");
+        line_at(x, "                                       ");
+        line_at(x, "                                       ");
+        sprintf(buff, "             %s~%s=[,,_,,]:3               ", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+        line_at(x, buff);
+        line_at(x, "                                       ");
+    } else {
+        line_at(x, "                                       ");
+        line_at(x, "                                       ");
+        sprintf(buff, "                                 %s*%s     ", ANSI_COLOR_BLUE, ANSI_COLOR_RESET);
+        line_at(x, buff); 
+        line_at(x, " .                                     ");
+        line_at(x, "             ~=[,,_,,]:3               ");
+    }
+
+    usleep(FRAME_TIME * 8);
+}
+
+void clear_car(int x)
+{
+    move_to_top();
+    line_at(x, "                                       ");
+    line_at(x, "                                       ");
+    line_at(x, "                                       ");
+    line_at(x, "                                       ");
+    line_at(x, "                                       ");
+    line_at(x, "                                       ");
+    line_at(x, "                                       ");
+}
